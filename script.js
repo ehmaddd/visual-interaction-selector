8e007ad54de29f5a6d2766fa2c04e247f0071cfd
ehmaddd <ehmaddd@gmail.com>
2023-11-06 21:09:14 +0500

    Add comments in script file

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-let isSelectionMode = false;
-let selectedElements = [];
-let interactionCounts = {};
+let isSelectionMode = false; //check selection mode
+let selectedElements = []; //collection of selected elements
+let interactionCounts = {}; //count element-wise interactions
 
+// Make sure document is loaded properly
 document.addEventListener("DOMContentLoaded", () => {
+    // Select all interactive elements
     const interactiveElements = document.querySelectorAll('.interactive-button, .interactive-link, .interactive-video, .interactive-dropdown');
+    // Select dashboard element
     const dashboard = document.getElementById('dashboard');
 
+    // apply click event listener to all the interactive elements
     interactiveElements.forEach(element => {
         element.addEventListener('click', () => {
+            // Perform selection/de-selection
             if (isSelectionMode) {
                 markElementForTracking(element);
             }
+            // trigger event for console logging
             simulateEvent(element);
+            // show updated dashboard contents
             updateDashboard(dashboard);
         });
     });
 
+    // Check whether s or S is pressed
     document.addEventListener('keydown', (event) => {
         if (event.key === 's' || event.key === 'S') {
             toggleSelectionMode();
@@ -23,6 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+// Activate/De-activate selection mode
 function toggleSelectionMode() {
     isSelectionMode = !isSelectionMode;
 
@@ -31,10 +40,12 @@ function toggleSelectionMode() {
         selectedElements.forEach(element => {
             element.style.borderColor = "transparent";
         });
+        // empty selection array
         selectedElements = [];
     }
 }
 
+// Select/De-select elements based on mode
 function markElementForTracking(element) {
     if (selectedElements.includes(element)) {
         // Deselect the element
@@ -48,7 +59,7 @@ function markElementForTracking(element) {
     }
 }
 
-
+// Check eventType and eventText and log on console
 function simulateEvent(element) {
     let elementType = '';
     let elementText = '';
@@ -73,6 +84,7 @@ function simulateEvent(element) {
     }
 }
 
+// Update contents of dashboard on every update
 function updateDashboard(dashboard) {
     dashboard.innerHTML = '<h2>Selected Elements Dashboard</h2>';
     for (const [elementText, count] of Object.entries(interactionCounts)) {
